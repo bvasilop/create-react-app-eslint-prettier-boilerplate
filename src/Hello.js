b7f025522d1689fecdd9bc1bb66b23f232b45166
Bill Vasilopoulos <billvasilopoulos@Bills-MBP-2.domain>
2019-11-17 16:10:54 -0800

    added project files

---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Hello = () => (
+  <div>
+    <h1>Hello World</h1>
+    <p>This is a create react app with eslint and prettier configurations</p>
+  </div>
+);
+export default Hello;
